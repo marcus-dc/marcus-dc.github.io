816502c847a8d120223c524970d6875a029f68e4
Marcus <techstacksph.marcus@gmail.com>
2023-04-06 17:26:45 +0800

    fix image in card not respecting border radius

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -3,7 +3,11 @@ import { FC, PropsWithChildren } from 'react';
 interface CardProps extends PropsWithChildren {}
 
 export const Card: FC<CardProps> = ({ children }) => {
-  return <div className="rounded-lg shadow bg-heroGradient">{children}</div>;
+  return (
+    <div className="overflow-hidden rounded-lg shadow bg-heroGradient">
+      {children}
+    </div>
+  );
 };
 
 interface CardContentProps extends PropsWithChildren {}
