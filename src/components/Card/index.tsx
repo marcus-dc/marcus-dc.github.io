f11d078383c4160ad6d5f1994142bd1314a1967f
Marcus <techstacksph.marcus@gmail.com>
2023-04-06 17:24:47 +0800

    add card component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,13 @@
+import { FC, PropsWithChildren } from 'react';
+
+interface CardProps extends PropsWithChildren {}
+
+export const Card: FC<CardProps> = ({ children }) => {
+  return <div className="rounded-lg shadow bg-heroGradient">{children}</div>;
+};
+
+interface CardContentProps extends PropsWithChildren {}
+
+export const CardContent: FC<CardContentProps> = ({ children }) => {
+  return <div className="p-3">{children}</div>;
+};
